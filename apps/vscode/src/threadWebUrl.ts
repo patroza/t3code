2a9b07fb9d4bba9234188125b4e857890e4f1bba
Patrick Roza <contact@patrickroza.com>
2026-07-26 10:27:46 +0200

    refactor: extract Discord and VS Code clients from fork trunk (#76)

--- apps/vscode/src/threadWebUrl.ts
+++
@@ -1,8 +0,0 @@
-export function threadWebUrl(httpBaseUrl: string, threadId: string): string {
-  const url = new URL(httpBaseUrl);
-  url.pathname = "/";
-  url.search = "";
-  url.searchParams.set("thread", threadId);
-  url.hash = "";
-  return url.toString();
-}
