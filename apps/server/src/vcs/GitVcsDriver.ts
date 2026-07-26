7a7cc493901b1e71e18e5f64ffa5552e884fe933
Tim Smart <hello@timsmart.co>
2026-07-26 12:25:49 +0200

    feat(tim): import tim-smart/t3code#5
    Load direnv environments for provider sessions
    
    Source: https://github.com/tim-smart/t3code/pull/5
    Source head: 8f5fc87c13f4628c179cda44d4f32f7fe4d316b2
    Source commits: e4f07014d39964fde2498bcb35588974cc5e6232,0d1463af61e0bd174f698b2519ebf3b207a2eaca,a66e4160d5f4b79140ec8fbcbc6aa66af750a991,8f5fc87c13f4628c179cda44d4f32f7fe4d316b2
    Imported: complete product delta from the source PR.
    
    (cherry picked from commit 0da8bfeb54f685ded6021801c87017fadf757175)

--- apps/server/src/vcs/GitVcsDriver.ts
+++ apps/server/src/vcs/GitVcsDriver.ts
@@ -30,6 +30,7 @@ import {
   type VcsStatusInput,
   type VcsStatusResult,
 } from "@t3tools/contracts";
+import * as DirenvEnvironment from "../provider/DirenvEnvironment.ts";
 import { makeGitVcsDriverCore } from "./GitVcsDriverCore.ts";
 import * as VcsDriver from "./VcsDriver.ts";
 import * as VcsProcess from "./VcsProcess.ts";
@@ -878,5 +879,9 @@ export const make = Effect.gen(function* () {
   return GitVcsDriver.of(git);
 });
 
-export const vcsLayer = Layer.effect(VcsDriver.VcsDriver, makeVcsDriver);
-export const layer = Layer.effect(GitVcsDriver, make);
+export const vcsLayer = Layer.effect(VcsDriver.VcsDriver, makeVcsDriver).pipe(
+  Layer.provide(DirenvEnvironment.layerLive),
+);
+export const layer = Layer.effect(GitVcsDriver, make).pipe(
+  Layer.provide(DirenvEnvironment.layerLive),
+);
